90b122df843c22ae401b68ef0250d2f25f6e5b41
Kadam-14 <kadamadityaanil@gmail.com>
2026-09-16 20:22:44 +0530

    Created Login Controller

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -28,3 +28,39 @@ module.exports.registerUser = async (req, res, next) => {
     res.status(201).json({ token,user });
 
 }
+
+module.exports.loginUser = async(req, res, next) => {
+
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        res.status(400).json({
+            errors: errors.array()
+        })
+    }
+
+    const {email , password } = req.body;
+
+    const user = await userModel.findOne({ email }).select('+password');
+
+    if(!user){
+        res.status(401).json({
+            message: 'Invalid Email or Password'
+        })
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if(!isMatch){
+        res.status(401).json({
+            message: "Invalid Password"
+        })
+    }
+
+    const token  = user.generateAuthToken();
+
+    res.status(200).json({
+        token, user
+    });
+    
+}
