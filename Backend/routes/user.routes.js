296e4ffd8ec6238b2b2ef28a575248528583a450
Kadam-14 <kadamadityaanil@gmail.com>
2026-09-16 20:42:20 +0530

    Modified Writing Implemenatation in user routes in the auth part

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -6,19 +6,33 @@ const { body } = require("express-validator")
 
 
 
-router.post("/register",[
-    body(('email').isEmail().withMessage('Invalid Email')),
-    body('fullname.firstname').isLength({ min : 3}),withMessage('First Name Should be atleast of 3 charactres'),
-    body('password').isLength({ min : 6}).withMessage('Password must be atleast of 6 characters')
-],
-   userControllers.registerUser 
-    )
+router.post("/register", [
 
-router.post("/login",[
-    body(('email').isEmail().withMessage('Invalid Email')),
-    body(('password').length().withMessage('Password Should Be Atleast of 6 characters'))
-],
-        userControllers.loginUser
-)
+    body('email')
+        .isEmail()
+        .withMessage('Invalid Email'),
+
+    body('fullname.firstname')
+        .isLength({ min: 3 })
+        .withMessage('First Name Should be at least 3 characters'),
+
+    body('password')
+        .isLength({ min: 6 })
+        .withMessage('Password must be at least 6 characters')
+
+], userControllers.registerUser);
+
+
+router.post("/login", [
+
+    body('email')
+        .isEmail()
+        .withMessage('Invalid Email'),
+
+    body('password')
+        .isLength({ min: 6 })
+        .withMessage('Password Should Be Atleast 6 characters')
+
+], userControllers.loginUser);
 
 module.exports = router;
