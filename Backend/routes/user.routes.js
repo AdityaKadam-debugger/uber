bad90e2cff3f154185717600dd84e5081725fac4
Kadam-14 <kadamadityaanil@gmail.com>
2026-09-16 20:03:44 +0530

    Created User Routes

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -6,7 +6,7 @@ const { body } = require("express-validator")
 
 
 
-router.post("/resgister",[
+router.post("/register",[
     body(('email').isEmail().withMessage('Invalid Email')),
     body('fullname.firstname').isLength({ min : 3}),withMessage('First Name Should be atleast of 3 charactres'),
     body('password').isLength({ min : 6}).withMessage('Password must be atleast of 6 characters')
